6493e3bb69ddc2d653b3f85e3eb2db01e8611f4d
vk7061iitb <vk4097440@gmail.com>
2023-07-23 08:13:30 +0530

    flutter catalog

--- flutter_catalog/lib/Pages/login_page.dart
+++ flutter_catalog/lib/Pages/login_page.dart
@@ -1,63 +1,152 @@
-import 'package:flutter/material.dart';
+import 'dart:js_interop';
 
-class LoginPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import '../utils/routes.dart';
+
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // In dart We make members private by using "_" before the name.
+
+  String name = "";
+  bool changeButton = false;
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    setState(() {
+      changeButton = true;
+    });
+    await Future.delayed(
+      const Duration(seconds: 1),
+    );
+    // ignore: use_build_context_synchronously
+    Navigator.pushNamed(context, MyRoutes.homeRoute);
+    await Future.delayed(
+      const Duration(seconds: 1),
+    );
+    setState(() {
+      changeButton = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
-        color: Colors.white,
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/images/login_image1.jpg",
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            const Text(
-              "Welcome",
-              style: TextStyle(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/6310507.jpg",
+            fit: BoxFit.cover,
+          ),
+//
+          const SizedBox(
+            height: 10.0,
+          ),
+//
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              "Welcome $name",
+              style: const TextStyle(
                 fontWeight: FontWeight.bold,
                 color: Colors.black,
                 fontSize: 24,
               ),
             ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+          ),
+//
+          const SizedBox(
+            height: 10.0,
+          ),
+//
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+            child: Form(
+              key: _formKey,
               child: Column(
                 children: [
                   TextFormField(
+                    validator: (value) {
+                      if (value.isNull) {
+                        "username cannot be empty";
+                      }
+                      return null;
+                    },
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                     decoration: const InputDecoration(
                       hintText: "Enter username",
                       labelText: "username",
                     ),
                   ),
+//
                   TextFormField(
                     obscureText: true,
+                    validator: (value) {
+                      if (value!.length < 6) {
+                        return "length of should be atleast 6";
+                      }
+                      return null;
+                    },
                     decoration: const InputDecoration(
                       hintText: "Enter Password",
                       labelText: "password",
                     ),
                   ),
+//
                   const SizedBox(
                     height: 20.0,
                   ),
-                  ElevatedButton(
-                    style: TextButton.styleFrom(),
-                    onPressed: () {
-                      // ignore: avoid_print
-                      print("Hi Codepur");
-                    },
-                    child: const Text("Login"),
-                  )
+//
+                  Material(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(50),
+//
+                    child: InkWell(
+                      onTap: () async {
+                        moveToHome(context);
+                      },
+//
+                      child: AnimatedContainer(
+                        duration: const Duration(seconds: 1),
+                        width: changeButton ? 50 : 150,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          shape: changeButton
+                              ? BoxShape.circle
+                              : BoxShape.rectangle,
+                        ),
+                        alignment: Alignment.center,
+//
+                        child: changeButton
+                            ? const Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+//
+                            : const Text(
+                                'Login',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                      ),
+                    ),
+                  ),
                 ],
               ),
-            )
-          ],
-        ));
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- flutter_catalog/lib/main.dart
+++ flutter_catalog/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/Pages/HomePage.dart';
 import 'package:flutter_catalog/Pages/login_page.dart';
+import 'package:flutter_catalog/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -13,14 +14,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
       theme: ThemeData(
           primarySwatch: Colors.red, fontFamily: GoogleFonts.lato().fontFamily),
       darkTheme: ThemeData(brightness: Brightness.dark),
       routes: {
         "/": (context) => const LoginPage(),
-        "/home": (context) => const HomePage(),
-        "/login": (context) => const LoginPage(),
+        MyRoutes.homeRoute: (context) => const HomePage(),
+        MyRoutes.loginRoute: (context) => const LoginPage(),
       },
     );
   }
