bc512737aaf0165c596880e4d0406e7d5ff48ea5
vk7061iitb <vk4097440@gmail.com>
2023-10-20 18:31:03 +0530

    second commit

--- E-Dialysis/lib/pages/home_page.dart
+++ E-Dialysis/lib/pages/home_page.dart
@@ -29,7 +29,7 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
       ),
 
-      bottomNavigationBar: MyGNav()
+      bottomNavigationBar: const MyGNav()
     );
   }
 }
