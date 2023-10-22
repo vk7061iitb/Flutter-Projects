9620fb1f02db9e557aceb3e9e412d64c828a01e2
vk7061iitb <vk4097440@gmail.com>
2023-10-22 15:46:20 +0530

    Added marker

--- assingment_1/lib/Pages/location_activity.dart
+++ assingment_1/lib/Pages/location_activity.dart
@@ -6,13 +6,13 @@ class LocationActivity extends StatefulWidget {
   const LocationActivity({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _LocationActivityState createState() => _LocationActivityState();
 }
 
 class _LocationActivityState extends State<LocationActivity> {
   late GoogleMapController mapController;
   LatLng? initialCameraPosition;
+  final Map<String, Marker> _markers = {};
 
   @override
   void initState() {
@@ -20,10 +20,8 @@ class _LocationActivityState extends State<LocationActivity> {
     _getCurrentLocation();
   }
 
-  // Function to get user's current location with his/her permission using Geolocator package
   Future<void> _getCurrentLocation() async {
     LocationPermission permission = await Geolocator.checkPermission();
-    // If the permission is denied then exit the function
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
@@ -35,7 +33,6 @@ class _LocationActivityState extends State<LocationActivity> {
       desiredAccuracy: LocationAccuracy.high,
     );
 
-    // Storing the latitude and longitude got from "_getcurrentposition" function in "initialcameraposition"
     setState(() {
       initialCameraPosition = LatLng(position.latitude, position.longitude);
     });
@@ -43,9 +40,11 @@ class _LocationActivityState extends State<LocationActivity> {
     // When the get location button is tapped then updating the Camera position
     if (initialCameraPosition != null) {
       mapController.animateCamera(
-      // Using animate camera for smooth movement of camera view
         CameraUpdate.newLatLng(initialCameraPosition!),
       );
+
+      // Add a marker after getting the location
+      addMarker('user', initialCameraPosition!);
     }
   }
 
@@ -54,20 +53,21 @@ class _LocationActivityState extends State<LocationActivity> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
-        title: const Text("Show My Location",
-        style: TextStyle(
-          fontSize: 22,
-          fontWeight: FontWeight.bold,
-        ),
+        title: const Text(
+          "Show My Location",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
       body: Stack(
         children: [
-          // Using Google map to display the map
           GoogleMap(
             onMapCreated: (controller) {
               mapController = controller;
             },
+            markers: _markers.values.toSet(),
             initialCameraPosition: CameraPosition(
               target: initialCameraPosition ?? const LatLng(0, 0),
               zoom: 15.0,
@@ -77,26 +77,40 @@ class _LocationActivityState extends State<LocationActivity> {
           Positioned(
             bottom: 22,
             left: 16,
-  
-            // Elevated Button to get the current location of the user when he/she taps on it
             child: ElevatedButton(
-             onPressed: _getCurrentLocation,
-             style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.red),
-              shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
+              onPressed: _getCurrentLocation,
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.red),
+                shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                ),
               ),
+              child: const Text(
+                'Get Location',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-             ),
-             child: const Text('Get Location',
-             style: TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.bold,
-             ),),
-             )
+            ),
           ),
         ],
       ),
     );
+  }
+
+// Add Marker Function to add marker at your current position
+  addMarker(String id, LatLng location) {
+    var marker = Marker(
+      markerId: MarkerId(id),
+      position: location,
+      infoWindow: const InfoWindow(
+        title: "Your Current Location",
+      ),
+    );
+
+    _markers[id] = marker;
+    setState(() {});
   }
 }
