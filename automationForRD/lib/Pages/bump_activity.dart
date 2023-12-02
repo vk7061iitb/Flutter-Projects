214d8c2db6d710d180358f287fb45183255b420e
vk7061iitb <vk4097440@gmail.com>
2023-12-02 17:57:04 +0530

    chage spline to line curve

--- automationForRD/lib/Pages/bump_activity.dart
+++ automationForRD/lib/Pages/bump_activity.dart
@@ -11,35 +11,54 @@ class BumpActvity extends StatefulWidget {
 }
 
 class _BumpActvityState extends State<BumpActvity> {
-  List<double> _accelerometerReading = [0];
-  final List<DataPoint> _accelerationData = [];
+  List<double> _accelerometerReading = [0, 0, 0];
+  // ignore: non_constant_identifier_names
+  final List<DataPoint> _accelerationData_X = [];
+  // ignore: non_constant_identifier_names
+  final List<DataPoint> _accelerationData_Y = [];
+  // ignore: non_constant_identifier_names
+  final List<DataPoint> _accelerationData_Z = [];
 
-// final List <DataPoint> velocityData = [];
   bool flagA = false;
-  bool flagB = false;
+  bool flagX = false;
+  bool flagY = false;
+  bool flagZ = false;
+  DateTime time1 = DateTime.now();
+  late TooltipBehavior _tooltipBehavior;
+
+
+   static const int windowSize = 20; // Number of data points to display in the window
+  // static const int updateInterval = 1000; // Update interval in milliseconds
 
   @override
   void initState() {
+    _tooltipBehavior  = TooltipBehavior(enable: true);
     super.initState();
     // Listen to accelerometer events
     userAccelerometerEvents.listen((UserAccelerometerEvent event) {
       if (mounted) {
         setState(() {
-          // Update accelerometer readings, rounded to one decimal place
-          _accelerometerReading = <double>[
-            event.z,
-          ];
-          flagB = (_accelerometerReading[0].abs() >= 0.1);
-          // Adding acceleration Datapoint for plotting
-          if (flagA && flagB) {
-            _accelerationData.add(DataPoint(
-                x: DateTime.now(), y: _accelerometerReading[0]));
+          if(_accelerationData_Z.length >= windowSize){
+            _accelerationData_Z.removeAt(0);
+          }
+          _accelerometerReading = <double>[event.x, event.y, event.z];
+          // Determine if any axis has significant acceleration
+          bool hasAcceleration =
+              _accelerometerReading.any((value) => value.abs() >= 0.1);
+          if (flagA && hasAcceleration) {
+            DateTime currentTime = DateTime.now();
+            _accelerationData_X.add(
+                DataPoint(x: currentTime, y: _accelerometerReading[0]));
+            _accelerationData_Y.add(
+                DataPoint(x: currentTime, y: _accelerometerReading[1]));
+            _accelerationData_Z.add(
+                DataPoint(x: currentTime, y: _accelerometerReading[2]));
           }
         });
       }
     });
   }
-  
+
   @override
   void dispose() {
     super.dispose();
@@ -60,37 +79,88 @@ class _BumpActvityState extends State<BumpActvity> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
+        child: ListView(
           children: [
             Padding(
               padding: const EdgeInsets.only(top: 10),
               child: SizedBox(
+                  height: 250,
+                  child: SfCartesianChart(
+                    tooltipBehavior: _tooltipBehavior,
+                    primaryXAxis: DateTimeAxis(
+                      isVisible: false,
+                      maximum: flagA? DateTime.now(): time1
+                    ),
+                    series: <ChartSeries>[
+                      LineSeries<DataPoint, DateTime>(
+                        enableTooltip: true,
+                        dataSource: _accelerationData_Z,
+                        xValueMapper: (DataPoint data, _) =>
+                            data.x,
+                        yValueMapper: (DataPoint data, _) => data.y,
+                        animationDuration: 1,
+                      )
+                    ],
+                    title: ChartTitle(text: 'a_z(Raw Data)'),
+                    borderColor: Colors.transparent,
+                    backgroundColor: Colors.transparent,
+                    enableAxisAnimation: true,
+                  )),
+            ),
+
+            /* Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: SizedBox(
                 height: 250,
                 child: SfCartesianChart(
-                  series: <ChartSeries> [
-                    SplineSeries<DataPoint, int>(
-                      dataSource: _accelerationData, 
-                      xValueMapper: (DataPoint data, _) => data.x.millisecondsSinceEpoch, 
+                  primaryXAxis: DateTimeAxis(
+                        isVisible: false,
+                        maximum: flagA? DateTime.now(): time1
+                      ),
+                  series: <ChartSeries>[
+                    SplineSeries<DataPoint, DateTime>(
+                      dataSource: _accelerationData_Y,
+                      xValueMapper: (DataPoint data, _) =>
+                          data.x,
                       yValueMapper: (DataPoint data, _) => data.y,
                       animationDuration: 1000,
-                      )
+                    )
                   ],
-
-                  title: ChartTitle(
-                    text: 'Bump Detection'
-                  ),
+                  title: ChartTitle(text: 'y-acceleration'),
                   borderColor: Colors.transparent,
                   backgroundColor: Colors.transparent,
-                  primaryXAxis: NumericAxis(
-                    isVisible: false,
-                  ),
-                  enableAxisAnimation: true,
                   
-                )
+                  enableAxisAnimation: true,
+                ),
               ),
             ),
 
-            // Start button 
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: SizedBox(
+                  height: 250,
+                  child: SfCartesianChart(
+                    primaryXAxis: DateTimeAxis(
+                        isVisible: false,
+                        maximum: flagA? DateTime.now(): time1
+                      ),
+                    series: <ChartSeries>[
+                      SplineSeries<DataPoint, DateTime>(
+                        dataSource: _accelerationData_Z,
+                        xValueMapper: (DataPoint data, _) =>
+                            data.x,
+                        yValueMapper: (DataPoint data, _) => data.y,
+                        animationDuration: 1000,
+                      )
+                    ],
+                    title: ChartTitle(text: 'z-acceleration'),
+                    borderColor: Colors.transparent,
+                    backgroundColor: Colors.transparent,
+                    enableAxisAnimation: true,
+                  )),
+            ), */
+
+            // Start button
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -99,10 +169,10 @@ class _BumpActvityState extends State<BumpActvity> {
                   child: ElevatedButton(
                       onPressed: () {
                         setState(() {
+                          _accelerationData_X.clear();
+                          _accelerationData_Y.clear();
+                          _accelerationData_Z.clear();
                           flagA = true;
-                          //startTime = DateTime.now().hour.toDouble();
-                          
-                          _accelerationData.clear();
                         });
                       },
                       style: ButtonStyle(
@@ -118,6 +188,7 @@ class _BumpActvityState extends State<BumpActvity> {
                 ElevatedButton(
                   onPressed: () {
                     setState(() {
+                      time1 = DateTime.now();
                       flagA = false;
                     });
                   },
@@ -139,7 +210,7 @@ class _BumpActvityState extends State<BumpActvity> {
   }
 }
 
-// Datapoint class to store time and Net acceleration value
+// Datapoint class to store time and acceleration value
 class DataPoint {
   DateTime x;
   final double y;
