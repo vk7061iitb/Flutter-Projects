c55830f97f095739360b65ec508b570d56ca1a21
vk7061iitb <vk4097440@gmail.com>
2024-01-06 14:00:38 +0530

    firebase emulator initialized properly

--- automationForRD/lib/main.dart
+++ automationForRD/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_logs/flutter_logs.dart';
 import 'package:pave_track_master/Presentation/Pages/acc_page.dart';
@@ -28,6 +30,15 @@ void main() async {
       logsExportDirectoryName: "MyLogs/Exported",
       debugFileOperations: true,
       isDebuggable: true);
+
+  // Use emulators only in debug mode
+  if (kDebugMode) {
+    FirebaseFirestore.instance.settings = const Settings(
+      host: 'localhost:8080',
+      sslEnabled: false,
+      persistenceEnabled: false,
+    );
+  }
   runApp(const MainApp());
 }
 
